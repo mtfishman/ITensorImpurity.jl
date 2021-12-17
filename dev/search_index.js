ab1f06406ff307c097cc07eee54d0114f6496ade
Documenter.jl <documenter@juliadocs.github.io>
2021-12-17 20:47:47 +0000

    build based on 896927a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ITensorImpurity","category":"page"},{"location":"#ITensorImpurity","page":"Home","title":"ITensorImpurity","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ITensorImpurity.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ITensorImpurity]","category":"page"}]
+}
